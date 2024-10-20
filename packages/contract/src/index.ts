debea35d7f4f0f0ff6074b2ca5fbddacbf628474
amaurycoudr <amaury.coudray@gmail.com>
2024-10-20 15:15:05 +0200

    chore(f): set up front app

--- packages/contract/src/index.ts
+++ packages/contract/src/index.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 const c = initContract();
 
 export const contract = c.router({
-  healthCheck: {
+  getHealthStatus: {
     path: "/health",
     method: "GET",
     responses: { 200: z.object({ status: z.string() }) },
