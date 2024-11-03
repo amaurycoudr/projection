c358a01903e3030acf6a15312f0b6d2209abf509
amaurycoudr <amaury.coudray@gmail.com>
2024-11-03 15:52:01 +0100

    feat(chess): set up the package chess

---
+++ packages/chess/eslint.config.js
@@ -0,0 +1,25 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      'prefer-template': 'error',
+      'no-unused-vars': 'off',
+      'no-console': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error', // or "error"
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+];
